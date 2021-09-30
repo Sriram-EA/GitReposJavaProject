ab3b468a8c3728dfdb7d152d4d06594c47c1d7d1
Sriram <easriram@gmail.com>
2021-10-01 01:13:55 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AddUserComponent,
+    EditUserComponent,
+    ListUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/list-user/list-user.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../User';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-list-user',
+  templateUrl: './list-user.component.html',
+  styleUrls: ['./list-user.component.css']
+})
+export class ListUserComponent implements OnInit {
+
+     users: User[];								
+								
+    constructor(private router: Router, private userService: UserService) { }								
+								
+    // Initialize with default list of users								
+    ngOnInit() {					
+
+      if(localStorage.getItem("username")!=null)
+      {								                       // Goes to user Service and gets the data
+        this.userService.getUsers()								
+        .subscribe(data=> {								
+        this.users = data;								
+          });								
+      }								
+      else								
+        this.router.navigate(['/login']);															
+      }							
+								
+      // logOff user								
+      logOutUser():void{								
+        if(localStorage.getItem("username")!=null){								
+          localStorage.removeItem("username");								
+          this.router.navigate(['/login']);								
+      }								
+  }				
+							
+//Modify USer								
+public editUser(user: User): void {								
+  localStorage.removeItem("editUserId");								
+  localStorage.setItem("editUserId", user.id.toString());								
+  this.router.navigate(['edit-user']);								
+};								
+								
+// //Add New User								
+addUser(): void {								
+  this.router.navigate(['add-user']);								
+};							
+}	
